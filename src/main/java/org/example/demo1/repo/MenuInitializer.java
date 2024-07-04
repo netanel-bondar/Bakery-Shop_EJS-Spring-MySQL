e51bd3f3811b3e447c44f48b0271b709c0be0ad8
Netanel Bondat <netanelbondar74@gmail.com>
2024-07-04 19:55:30 +0300

    deleted old way of creating the menu

--- src/main/java/org/example/demo1/repo/MenuInitializer.java
+++ src/main/java/org/example/demo1/repo/MenuInitializer.java
@@ -35,36 +35,6 @@ public class MenuInitializer implements CommandLineRunner {
             repositoryFood.save(new Food((long) i+1, foodName,
                     category, price));
         }
-
-//        if (repositoryFood.count() == 0) {
-//            repositoryFood.save(new Food(1L,"Butter Croissant", "Pastries", 30));
-//            repositoryFood.save(new Food(2L,"Chocolate croissant", "Pastries", 31));
-//            repositoryFood.save(new Food(3L,"Eclair", "Pastries",17));
-//            repositoryFood.save(new Food(4L,"Cinnamon Roll", "Pastries",29));
-//            repositoryFood.save(new Food(5L,"Tart - varies according to the season", "Pastries",45));
-//            repositoryFood.save(new Food(6L,"A Dreamy Morning Shakshuka", "Breakfast",55));
-//            repositoryFood.save(new Food(7L,"Scrambled egg Croissant", "Breakfast",48));
-//            repositoryFood.save(new Food(8L,"Corruption Toast", "Breakfast",48));
-//            repositoryFood.save(new Food(9L,"Pancake Lighthouse", "Breakfast",50));
-//            repositoryFood.save(new Food(10L,"Heart shaped chocolate cake", "Cakes",95));
-//            repositoryFood.save(new Food(11L,"Birthday cake", "Cakes",255));
-//            repositoryFood.save(new Food(12L,"Festive cheesecake", "Cakes",290));
-//            repositoryFood.save(new Food(13L,"Saint Honore cake", "Cakes",180));
-//            repositoryFood.save(new Food(14L,"Lemon Tart", "Desserts",42));
-//            repositoryFood.save(new Food(15L,"Pecan Pie", "Desserts",42));
-//            repositoryFood.save(new Food(16L,"White Chocolate Carrot Cake", "Desserts",35));
-//            repositoryFood.save(new Food(17L,"Red Volt", "Desserts",35));
-//            repositoryFood.save(new Food(18L,"A Strip of seven Macarons", "Desserts",65));
-//            repositoryFood.save(new Food(19L,"Espresso", "HotDrinks",9));
-//            repositoryFood.save(new Food(20L,"Americano", "HotDrinks",12));
-//            repositoryFood.save(new Food(21L,"Hot Chocolate", "HotDrinks",17));
-//            repositoryFood.save(new Food(22L,"Cold Americano", "ColdDrinks",15));
-//            repositoryFood.save(new Food(23L,"Cold Chocolate Drink", "ColdDrinks",13));
-//            repositoryFood.save(new Food(24L,"Water", "SoftDrinks",9));
-//            repositoryFood.save(new Food(25L,"Soda", "SoftDrinks",9));
-//            repositoryFood.save(new Food(26L,"Orange Juice", "FreshJuices",18));
-//            repositoryFood.save(new Food(27L,"Carrot Juice", "FreshJuices",18));
-//        }
     }
 
     private String getCategory(int i) {
